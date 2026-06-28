84e5c3e03c2fb9b91785a98ac221a6a3d8f492e4
Ali Malik <alimalikwevdeveloper@gmail.com>
2026-06-28 20:21:29 +0700

    practice of boolean data types

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-const userName = "Ali";
-let studentAge = 22;
-const isStudent = true;
-let email;
-let profilePicture = null;
 
-console.log(typeof userName)
-console.log(typeof studentAge)
-console.log(typeof isStudent)
-console.log(typeof email)
-console.log(typeof profilePicture)
+const hasPurchased = false;
+const hasPurchasedcourse = true;
+let watchingLessons = false;
+watchingLessons = true;
+console.log(hasPurchased)
+console.log(hasPurchasedcourse)
+console.log(watchingLessons)
 
+let isResturantopen = false;
+let isRideravailble = false;
+let isrderPlaced = false;
+
+console.log(isResturantopen)
+console.log(isRideravailble)
+console.log(isrderPlaced)
+
+let isContactformSubmitted = true;
+let hasUploudedfile = false;
+let isEmailverified = true;
+
+console.log(isContactformSubmitted)
+console.log(hasUploudedfile)
+console.log(isEmailverified)
+
+let isFollowing = true;
+let isonline = false;
+let isProfilePublic = true;
+
+
+console.log(isFollowing)
+console.log(isonline)
+console.log(isProfilePublic)
+
+let isWebsitelived = false;
+const isPaymentRecived = true;
+let hasMaintainnceplan = true;
+
+console.log(isWebsitelived)
+console.log(isPaymentRecived)
+console.log(hasMaintainnceplan)
