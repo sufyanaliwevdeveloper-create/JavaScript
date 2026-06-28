4c53fd1f2c136fe779611f5477f16747554ff1c3
Ali Malik <alimalikwevdeveloper@gmail.com>
2026-06-28 19:15:46 +0700

    practice of data types

--- script.js
+++ script.js
@@ -1,55 +1,12 @@
-const name = "Ali";
-const country = "Pakistan";
-let age = 22;
-age = "23";
+const userName = "Ali";
+let studentAge = 22;
+const isStudent = true;
+let email;
+let profilePicture = null;
 
-console.log(name)
-console.log(country)
-console.log(age)
-
-const websiteName = "Ali Store";
-const productName = "Wireless Mouse";
-let cart = 1;
-
-cart = 3;
-
-console.log(websiteName)
-console.log(productName)
-console.log(cart)
-
-const developerName = "Ali";
-let developerSpecialization = "MERN";
-let developerLocation = "Lahore";
-let developerExperiance = 3;
-developerExperiance = 4;
-console.log(developerName)
-console.log(developerSpecialization)
-console.log(developerLocation)
-console.log(developerExperiance)
-
-const courseName = "Web Developemnt";
-const instructorName = "Saad";
-const totalLessons = 30;
-let completedLessons = 0;
-completedLessons = 5;
-
-console.log(courseName)
-console.log(instructorName)
-console.log(totalLessons)
-console.log(completedLessons)
-
-const gameName = "Subway Surfer";
-const playerName = "Raza";
-let score = 0;
-console.log(gameName)
-console.log(playerName)
-console.log(score)
-score = 10;
-console.log(score)
-score = 25;
-console.log(score)
-score = 50;
-console.log(score)
-score = 100;
-console.log(score)
+console.log(typeof userName)
+console.log(typeof studentAge)
+console.log(typeof isStudent)
+console.log(typeof email)
+console.log(typeof profilePicture)
 
