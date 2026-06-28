68afc559ab68edb365afbbf18acb0626fb0d3fe1
Ali Malik <alimalikwevdeveloper@gmail.com>
2026-06-28 18:55:14 +0700

    practice of let and const

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
 const name = "Ali";
 const country = "Pakistan";
 let age = 22;
+age = "23";
 
 console.log(name)
 console.log(country)
 console.log(age)
+
+const websiteName = "Ali Store";
+const productName = "Wireless Mouse";
+let cart = 1;
+
+cart = 3;
+
+console.log(websiteName)
+console.log(productName)
+console.log(cart)
+
+const developerName = "Ali";
+let developerSpecialization = "MERN";
+let developerLocation = "Lahore";
+let developerExperiance = 3;
+developerExperiance = 4;
+console.log(developerName)
+console.log(developerSpecialization)
+console.log(developerLocation)
+console.log(developerExperiance)
+
+const courseName = "Web Developemnt";
+const instructorName = "Saad";
+const totalLessons = 30;
+let completedLessons = 0;
+completedLessons = 5;
+
+console.log(courseName)
+console.log(instructorName)
+console.log(totalLessons)
+console.log(completedLessons)
+
+const gameName = "Subway Surfer";
+const playerName = "Raza";
+let score = 0;
+console.log(gameName)
+console.log(playerName)
+console.log(score)
+score = 10;
+console.log(score)
+score = 25;
+console.log(score)
+score = 50;
+console.log(score)
+score = 100;
+console.log(score)
+
