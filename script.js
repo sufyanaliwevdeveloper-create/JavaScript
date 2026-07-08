687f99e4a8a1f48b13c61daad8fe3fc404fb5d16
Ali Malik <alimalikwevdeveloper@gmail.com>
2026-07-08 19:26:56 +0700

    hy to kese ho ap sub

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-let userLoggedIn = true;
-let haveSubscription = true;
+let linkedinLeads = 15;
+let referelLeads = 5;
 
-if (userLoggedIn && haveSubscription) {
-    console.log("Primium Content Dikhao")
+if (linkedinLeads + referelLeads >= 50) {
+    console.log("Goal Achived")
+}
+else if (linkedinLeads + referelLeads >= 30) {
+    console.log("Good Progress")
 }
 else{
-    console.log("Acces Denied Kero")
+    console.log("Still Need More outreach")
 }
 
-let marks = 58;
+let age = 18;
+let coursePurchased = false;
+let emailVerified = false;
 
-if (marks>=80) {
-    console.log("A Grade")
+if (age >= 18 , coursePurchased !== emailVerified) {
+    console.log("Start Your Course")
 }
-else if (marks>=60) {
-    console.log("B Grade")
-    
+
+else if (coursePurchased === emailVerified , age >= 18) {
+    console.log("Verified your Email")   
 }
+
 else{
-    
-    console.log("Sorry you are fail")
+    console.log("Acces Denied")   
 }
 
-let productStock = 0;
+let productPrice = 1000;
+let productQuantity = 4;
+let availableStock = 10;
+let minimumShoppingAmountForFreeShipping = 5000;
 
-if (productStock>0) {
-    console.log("In Stock")
+if (productPrice * productQuantity <= minimumShoppingAmountForFreeShipping, productQuantity < availableStock) {
     
+    console.log("Congratulations! You are eligible for our free shipping program")   
 }
-else{
+
+else if (productPrice * 4, productQuantity < availableStock) {
     
-    console.log("out of stock")
+    console.log("Your order ready")   
 }
